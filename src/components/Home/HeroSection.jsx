4041b3eb638bc7b75da024f5522c2a5eaf269845
Nikhil <nik21@Nikhils-MacBook-Air.local>
2025-04-01 15:39:45 +0530

    added navbar changes and responsiveness

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -8,37 +8,37 @@ import { TextGenerateEffect } from "../ui/TextGenerateEffect";
 
 const HeroSection = () => {
   return (
-    <div className="w-full h-[100vh] flex flex-col justify-center items-start bg-white">
+    <div className="w-full pt-[9rem] pb-[5rem] md:py-[13rem] lg:py-[13rem]  flex flex-col justify-center  items-start bg-white">
       <img
         src={"/HomeImages/people.png"}
         alt="img"
-        className="absolute bg-cover z-2 right-[-5rem] w-[46rem]"
+        className="absolute  invisible md:visible z-[-2rem] md:z-2 lg:z-2 right-[-5rem] w-[46rem]"
       />
       <img
         src={"/HomeImages/citybg.png"}
         alt="img"
-        className="absolute bg-auto z-[1] right-[-40rem] bottom-[-25rem] h-[200%]"
+        className="absolute bg-auto z-[1] right-[-40rem] bottom-[-31rem] h-[200%]"
       />
-      <div className="w-[65%] pl-[7rem] z-2 ">
+      <div className="w-full md:w-[65%] lg:w-[65%] px-4 md:px-[7rem] lg:px-[7rem] z-2 ">
         <div className=" w-full">
           <TextGenerateEffect
             words={"Find Anyone, Anytime"}
             className="text-[40px] md:text-6xl lg:text-7xl"
           />
-          <p className="text-gray-600 px-3 pr-[6rem]">
+          <p className="text-gray-600 px-3 pr-0 md:pr-[6rem] lg:pr-[6rem]">
             "Our platform connects you with the people and professionals you
             need—right in your neighborhood. Whether you're looking for a
             doctor, mechanic, engineer, or even a new friend, we make it easy to
             find and connect with trusted individuals nearby."
           </p>
         </div>
-        <div className="w-full flex flex-row justify-start items-center pl-3 gap-[3rem] mt-[3rem]">
-          <button className="bg-[#003b95] text-md font-bold rounded-[2rem] px-5 py-3 hover:bg-gray-800 cursor-pointer transition-all flex  text-gray-100 items-center gap-2">
-            <FaApple size={25} />
+        <div className="w-full flex flex-row justify-start items-center pl-3 gap-[2rem] mt-[3rem]">
+          <button className="bg-[#003b95] text-sm md:text-md font-bold rounded-[2rem] px-5 py-3 hover:bg-gray-800 cursor-pointer transition-all flex  text-gray-100 items-center gap-2">
+            <FaApple size={20} />
             App Store
           </button>
-          <button className="bg-[#003b95] text-md font-bold rounded-[2rem] px-5 py-3 hover:bg-gray-800 cursor-pointer transition-all flex text-gray-100  items-center gap-2">
-            <BiLogoPlayStore size={25} />
+          <button className="bg-[#003b95] text-sm md:text-md font-bold rounded-[2rem] px-5 py-3 hover:bg-gray-800 cursor-pointer transition-all flex text-gray-100  items-center gap-2">
+            <BiLogoPlayStore size={20} />
             Play Store
           </button>
         </div>
--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -3,53 +3,149 @@ import Link from "next/link";
 
 import { animationOnce } from "@/Configs/config";
 import { motion } from "framer-motion";
+import { BiLogoPlayStore } from "react-icons/bi";
+import { FiMenu } from "react-icons/fi";
+import { useState } from "react";
+import { MdKeyboardArrowRight } from "react-icons/md";
+
 
 
 const Navbar = () => {
-  return (
-    <nav className="w-[100vw] bg-transparent flex justify-center items-center fixed z-10 top-0">
-      <motion.div
-        initial={{ x: 0, y: "-13vh", opacity: 0 }} // Initially, clip the whole element
-        whileInView={{
-          y: 0,
-          x: 0,
-          opacity: 1,
-        }}
-        viewport={{ once: animationOnce }}
-        transition={{ duration: 0.6, ease: "easeOut" }} // Animation duration
-        style={{ transformOrigin: "top right" }}
-        className="h-[4rem] w-[30%] rounded-3xl   bg-gray-800 absolute top-9 z-8 flex flex-col md:flex-row lg:flex-row justify-center items-center overflow-hidden"
-      >
 
-          <div className="w-[100%] h-full  flex justify-center items-center  gap-4 md:gap-10 lg:gap-10">
+  const [menu, setMenu] = useState(false);
+
+  return (
+    <>
+      <nav className="w-[100vw] flex justify-center items-center fixed z-10 top-0">
+        <div className="h-[4rem] w-full bg-white border-b-2 py-[2.7rem]  top-0 z-8 flex flex-row md:flex-row lg:flex-row justify-center items-center overflow-hidden">
+          <div className=" w-[100%] md:w-[20%] lg:w-[20%]  h-full  flex justify-start pl-6 items-center  gap-1">
+            <img
+              src={"/logo.png"}
+              alt="logo"
+              className="w-[2.3rem] bg-blend-color-dodge"
+            />
             <Link
               href={"/"}
-              className="text-white hover:text-gray-300 transition-all cursor-pointer text-[0.9rem] font-bold"
+              className="text-gray-600 font-sans  hover:text-gray-300 transition-all cursor-pointer text-[1.3rem] font-semibold"
             >
-              LIVESPHERE
+              LiveSphere
             </Link>
+          </div>
+          <div className="w-[60%] h-full hidden md:visible lg:visible  md:flex lg:flex justify-center items-center  gap-4 md:gap-15 lg:gap-15">
             <Link
               href={"/about"}
-              className="text-gray-100 hover:text-gray-400 transition-all  text-sm"
+              className="text-gray-600 hover:text-gray-400 transition-all  text-sm font-normal"
             >
               About
             </Link>
             <Link
               href={"/blog"}
-              className="text-gray-100 hover:text-gray-400 transition-all  text-sm"
+              className="text-gray-600 hover:text-gray-400 transition-all px-4 py-[6] rounded-[0.8rem] bg-[#d1ddee] text-sm font-normal"
             >
               Blog
             </Link>
             <Link
               href={"/contact"}
-              className="text-gray-100 hover:text-gray-400 transition-all  text-sm"
+              className="text-gray-600 hover:text-gray-400 transition-all  text-sm font-normal"
             >
               Contact
             </Link>
           </div>
-      </motion.div>
-    </nav>
+          <div className="w-[50%] md:w-[20%] lg:w-[20%]h-full hidden md:visible lg:visible  md:flex lg:flex justify-end pr-6 items-center">
+            <button className="bg-[#003b95] text-sm font-normal rounded-[0.7rem] px-[13] py-2 hover:bg-gray-800 cursor-pointer transition-all flex text-gray-100  items-center gap-2">
+              Try for free
+              <BiLogoPlayStore size={20} />
+            </button>
+          </div>
+          <button
+            onClick={() => setMenu((pre) => !pre)}
+            className="bg-[#003b95] text-sm font-normal visible md:hidden lg:hidden rounded-[0.7rem] px-[13] py-2 mr-6 hover:bg-gray-800 cursor-pointer transition-all flex text-gray-100  items-center gap-2"
+          >
+            <FiMenu size={20} />
+          </button>
+        </div>
+      </nav>
+      {menu && (
+        <section className="w-[45%] shadow-2xs h-full bg-[#d1ddee] fixed md:hidden lg:hidden top-0 right-0 z-20 py-6">
+          <div className="h-[10%] w-full pl-6">
+            <button
+              onClick={() => setMenu((pre) => !pre)}
+              className="bg-[#003b95] text-sm font-normal visible md:hidden lg:hidden rounded-[0.7rem] px-[13] py-2 mr-6 hover:bg-gray-800 cursor-pointer transition-all flex text-gray-100  items-center gap-2"
+            >
+              <MdKeyboardArrowRight size={20} />
+            </button>
+          </div>
+          <div className="w-full h-full  flex flex-col md:hidden lg:hidden justify-start items-center  gap-2 md:gap-15 lg:gap-15">
+            <Link
+              href={"/about"}
+              className="text-gray-600 hover:text-gray-400 flex items-center justify-center transition-all w-full px-4 py-[6] bg-gray-200 text-md font-semibold"
+            >
+              About
+            </Link>
+            <Link
+              href={"/blog"}
+              className="text-gray-600 hover:text-gray-400 flex items-center justify-center transition-all w-full px-4 py-[6] bg-gray-200 text-md font-semibold"
+            >
+              Blog
+            </Link>
+            <Link
+              href={"/contact"}
+              className="text-gray-600 hover:text-gray-400 flex items-center justify-center transition-all w-full px-4 py-[6] bg-gray-200 text-md font-semibold"
+            >
+              Contact
+            </Link>
+          </div>
+        </section>
+      )}
+    </>
   );
 };
+
+// const Navbar = () => {
+//   return (
+//     <nav className="w-[100vw] bg-transparent flex justify-center items-center fixed z-10 top-0">
+//       <motion.div
+//         initial={{ x: 0, y: "-13vh", opacity: 0 }} // Initially, clip the whole element
+//         whileInView={{
+//           y: 0,
+//           x: 0,
+//           opacity: 1,
+//         }}
+//         viewport={{ once: animationOnce }}
+//         transition={{ duration: 0.6, ease: "easeOut" }} // Animation duration
+//         style={{ transformOrigin: "top right" }}
+//         className="h-[4rem] w-[30%] rounded-3xl   bg-gray-800 absolute top-9 z-8 flex flex-col md:flex-row lg:flex-row justify-center items-center overflow-hidden"
+//       >
+
+//           <div className="w-[100%] h-full  flex justify-center items-center  gap-4 md:gap-10 lg:gap-10">
+//             <Link
+//               href={"/"}
+//               className="text-white hover:text-gray-300 transition-all cursor-pointer text-[0.9rem] font-bold"
+//             >
+//               LIVESPHERE
+//             </Link>
+//             <Link
+//               href={"/about"}
+//               className="text-gray-100 hover:text-gray-400 transition-all  text-sm"
+//             >
+//               About
+//             </Link>
+//             <Link
+//               href={"/blog"}
+//               className="text-gray-100 hover:text-gray-400 transition-all  text-sm"
+//             >
+//               Blog
+//             </Link>
+//             <Link
+//               href={"/contact"}
+//               className="text-gray-100 hover:text-gray-400 transition-all  text-sm"
+//             >
+//               Contact
+//             </Link>
+//           </div>
+//       </motion.div>
+//     </nav>
+//   );
+// };
 
 export default Navbar;
